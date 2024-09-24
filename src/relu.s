cf738a3f4480a5614671749ac19f6e4a15cb3de4
Ezra Tramble <eztramble@berkeley.edu>
2024-09-24 00:42:25 -0700

    relu

--- src/relu.s
+++ src/relu.s
@@ -14,34 +14,31 @@
 # ==============================================================================
 relu:
     # Prologue
-    li t0, 1   
-    blt a1, t0, exit  
-    add x1, x2, x0
-    add t1, x0, x0 #counter
-    addi t2, a1, 0 #limit
-    
+    li t0, 1                
+    blt a1, t0, error_terminate  
+
+    li t1, 0                
+
 loop_start:
-    beq t1, t2, loop_end #if counter == limit -> loop_end
-    slli t3, t1, 2 
-    add t4, a0, t3
-    lw t5, 0(t4)
-    
-    blt t5, x0, loop_continue
-    
-    addi t1, t1, 1
-    j loop_start 
+    slli t2, t1, 2          
+    add t3, a0, t2         
+    lw t4, 0(t3)            
+    blt t4, x0, set_zero    
+
+    sw t4, 0(t3)            
+    j loop_continue         
+
+set_zero:
+    sw x0, 0(t3)            
 
 loop_continue:
-    li t5, 0
-    sw t5, 0(t4)
-    addi t1, t1, 1
-    j loop_start 
+    addi t1, t1, 1          
+    blt t1, a1, loop_start 
 
 loop_end:
     # Epilogue
+    jr ra                   
 
-    jr ra    
-    
-exit:
-    li a0 36
-    j exit
+error_terminate:
+    li a0, 36               
+    ecall                   
