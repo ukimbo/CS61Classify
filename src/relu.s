0c05a77ee6209e760cdb04ba5272dc068b11cd53
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-09-24 00:16:10 -0700

    Task 3 finished

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,34 @@
 # ==============================================================================
 relu:
     # Prologue
-
-
+    li t0, 1   
+    blt a1, t0, exit  
+    add x1, x2, x0
+    add t1, x0, x0 #counter
+    addi t2, a1, 0 #limit
+    
 loop_start:
-
-
-
-
-
-
-
+    beq t1, t2, loop_end #if counter == limit -> loop_end
+    slli t3, t1, 2 
+    add t4, a0, t3
+    lw t5, 0(t4)
+    
+    blt t5, x0, loop_continue
+    
+    addi t1, t1, 1
+    j loop_start 
 
 loop_continue:
-
-
+    li t5, 0
+    sw t5, 0(t4)
+    addi t1, t1, 1
+    j loop_start 
 
 loop_end:
-
-
     # Epilogue
 
-
-    jr ra
+    jr ra    
+    
+exit:
+    li a0 36
+    j exit
