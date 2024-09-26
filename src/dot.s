089e846a82229269e83c3cbf0d3469ac4b6152bd
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-09-25 18:04:28 -0700

    Dot product progress'

--- src/dot.s
+++ src/dot.s
@@ -20,25 +20,39 @@
 dot:
 
     # Prologue
-
-
+    li t0, 1
+    blt a2, t0, error_36
+    blt a3, t0, error_37
+    blt a4, t0, error_37
+    
+    li t0, 0 # workingSum
+    li t1, 0 # loopCounter
+    mv t2, a0 # array0 counter
+    mv t3, a1 # array1 counter
+    
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
+    beq t1, a1, loop_end
+    lw t4, 0(t2)
+    lw t5, 0(t3)
+    add t0, t4, t5
+    
+    
+    add t2, t2, a0
+    add t3, t3, a1
+    
+    addi t1, t1, 1
+    j loop_start
 
 loop_end:
 
-
     # Epilogue
-
-
+    mv a0, t0
     jr ra
+    
+error_36:
+    li a0, 36
+    ecall
+    
+error_37:
+    li a0, 37
+    ecall
