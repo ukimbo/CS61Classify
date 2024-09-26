a6daa9a3180b23a0b169e2b483f531b1aaf4c21c
Menimobong Utibe-Ukim <menimobong@berkeley.edu>
2024-09-25 17:18:35 -0700

    Working argmax function

--- src/argmax.s
+++ src/argmax.s
@@ -16,15 +16,34 @@
 # =================================================================
 argmax:
     # Prologue
+    li t0, 1                
+    blt a1, t0, error_terminate  
 
+    li t1, 0 #loop counter
+    li t2, 0 #currentMaxIndex
+    li t6, 0 #currentMax
 
 loop_start:
+    beq t1, a1, loop_end
+    slli t3, t1, 2          
+    add t4, a0, t3         
+    lw t5, 0(t4)
+    blt t6, t5, max
+    
+    j loop_continue         
 
+max:
+    mv t6, t5
+    mv t2, t1 
 
 loop_continue:
-
+    addi t1, t1, 1          
+    blt t1, a1, loop_start 
 
 loop_end:
     # Epilogue
-
     jr ra
+    
+error_terminate:
+    li a0, 36               
+    ecall   
